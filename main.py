d1891957c79a5976dde350c135fec38e8f2ae9e0
samuel33440-cmd <samuel33440@gmail.com>
2025-10-20 22:47:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-main.py
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Connecté comme {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("👋 Salut ! Je suis ton bot Discord hébergé sur Render.")
+
+bot.run(os.getenv("TOKEN"))
